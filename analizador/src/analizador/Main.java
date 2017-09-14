4adcf45d4ad9acfbe03d32c733d706c4af4da729
Estuche77 <96jaslat@gmail.com>
2017-09-14 10:45:40 -0600

    Limpaindo codigo v2

--- analizador/src/analizador/Main.java
+++ analizador/src/analizador/Main.java
@@ -19,7 +19,7 @@ public class Main {
     static List<Identificador> palabrasReservadas;
     static List<Identificador> literales;
     static List<Identificador> errores;
-    
+
     public static void main(String[] args) throws IOException {
         //String path = "C:/Users/MaríaLaura/Desktop/analizador_1/src/analizador/Lexer.flex";
         String path = "C:/Users/Estuche/Dropbox/NetBeansProjects/Scanner/analizador/src/analizador/Lexer.flex";
@@ -27,77 +27,78 @@ public class Main {
         probarLexerFile();
 
     }
-    
-    public static void generarLexer(String path){
-        File file=new File(path);
+
+    public static void generarLexer(String path) {
+        File file = new File(path);
         jflex.Main.generate(file);
     }
-    
-    public static void probarLexerFile() throws IOException{
-        int linea=1;
+
+    public static void probarLexerFile() throws IOException {
+        int linea = 1;
         tokenslist = new LinkedList<Identificador>();
         operadores = new LinkedList<Identificador>();
         identificadores = new LinkedList<Identificador>();
         palabrasReservadas = new LinkedList<Identificador>();
         literales = new LinkedList<Identificador>();
         errores = new LinkedList<Identificador>();
-        Identificador tokenitem=new Identificador();
-        
+        Identificador tokenitem = new Identificador();
+
         Reader reader = new BufferedReader(new FileReader("fichero.txt"));
-        Lexer lexer = new Lexer (reader);
-        
+        Lexer lexer = new Lexer(reader);
+
         while (true) {
-            
+
             Token token = lexer.yylex();
-            
-            if (token == null)
+
+            if (token == null) {
                 break;
-            
+            }
+
             System.out.println(token);
-            
-            switch (token){
+
+            switch (token) {
                 case SaltoDeLinea:
                     linea++;
                     break;
                 case ERROR:
-                    tokenitem.nombre=lexer.lexeme;
-                    tokenitem.ID=linea;
+                    tokenitem.nombre = lexer.lexeme;
+                    tokenitem.ID = linea;
                     errores.add(tokenitem);
                     break;
                 case LlaveI: {
-                    tokenitem.nombre=lexer.lexeme;
-                    tokenitem.ID=linea;
+                    tokenitem.nombre = lexer.lexeme;
+                    tokenitem.ID = linea;
                     operadores.add(tokenitem);
                     break;
                 }
                 case ParentesisI: {
-                    tokenitem.nombre=lexer.lexeme;
-                    tokenitem.ID=linea;
+                    tokenitem.nombre = lexer.lexeme;
+                    tokenitem.ID = linea;
                     operadores.add(tokenitem);
                     break;
                 }
                 case ParentesisD: {
-                    tokenitem.nombre=lexer.lexeme;
-                    tokenitem.ID=linea;
+                    tokenitem.nombre = lexer.lexeme;
+                    tokenitem.ID = linea;
                     operadores.add(tokenitem);
                     break;
                 }
                 case PalabraReservada: {
-                    tokenitem.nombre=lexer.lexeme;
-                    tokenitem.ID=linea;
+                    tokenitem.nombre = lexer.lexeme;
+                    tokenitem.ID = linea;
                     palabrasReservadas.add(tokenitem);
                     break;
                 }
                 case ID: {
-                    
-                    tokenitem.nombre=lexer.lexeme;
-                    tokenitem.ID=linea;
+
+                    tokenitem.nombre = lexer.lexeme;
+                    tokenitem.ID = linea;
                     tokenslist.add(tokenitem);
                     break;
                 }
                 case INT:
-                    tokenitem.nombre=lexer.lexeme;
-                    tokenitem.ID=linea;
+                    tokenitem.nombre = lexer.lexeme;
+                    tokenitem.ID = linea;
                     literales.add(tokenitem);
                     break;
             }
